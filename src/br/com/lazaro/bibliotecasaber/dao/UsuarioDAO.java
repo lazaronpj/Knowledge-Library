7ef6202eea2f7db60d9d99f6332fa9c8c225eacf
Lazaro Nogueira Pedro Junior <lzjunior.007@gmail.com>
2026-07-31 23:43:49 -0300

    UsuarioDAO - método de cadastrar usuário no banco

--- src/br/com/lazaro/bibliotecasaber/dao/UsuarioDAO.java
+++ src/br/com/lazaro/bibliotecasaber/dao/UsuarioDAO.java
@@ -1,8 +1,29 @@
 package br.com.lazaro.bibliotecasaber.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.lazaro.bibliotecasaber.bd.Banco;
+import br.com.lazaro.bibliotecasaber.dto.UsuarioDTO;
+
 public class UsuarioDAO {
 
-	public void cadastrarUsuario() {
+	public boolean cadastrarUsuario(UsuarioDTO usr) {
+		String sql = "INSERT INTO usuario (nome, email, senha, telefone) VALUES (?, ?, ?, ?)";
+		try(Connection cnx = new Banco().conectaBD();
+				PreparedStatement stmt = cnx.prepareStatement(sql)){
+			stmt.setString(1, usr.getNome());
+			stmt.setString(2, usr.getEmail());
+			stmt.setString(3, usr.getSenha());
+			stmt.setString(4, usr.getTelefone());
+			return stmt.executeUpdate() > 0;
+			
+		} catch(SQLException e) {
+			System.err.println("Erro ao cadastrar usuário no banco de dados!");
+			
+			return false;
+		}
 		
 	}
 }
